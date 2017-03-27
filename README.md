9d79b8b8f447252e89ed46dfee27d63217f0a97b
Jake Priddy <priddyjacob84@gmail.com>
2017-03-26 22:46:46 -0400

    completed script to display distance in cm in serial monitor

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ultrasonic_Sensor
+
+
+Components
+----------
+ - Arduino Uno
+ - HC-SR04 sensor
+ 
+Sensor Datasheet
+-----------------
+ - https://docs.google.com/document/d/1Y-yZnNhMYy7rwhAgyL_pfa39RsB-x2qR4vP8saG73rE/edit
+ 
--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+// define trigger and echo pins
+#define trigPin 13
+#define echoPin 12
 
+void setup() {
+  Serial.begin(9600); //Start serial monitor for debugging and display 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //create a 10 second pulse to measure distance 
+  long duration, distance;
+   digitalWrite(trigPin, LOW); 
+   delayMicroseconds(2);
+   digitalWrite(trigPin,HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigPin,LOW);
+   duration = pulseIn(echoPin, HIGH);
+    
+    // Distance based on the speed of sound at 68 degrees fahrenheit (343.5 m/s)
+    // (343.5 m/s = 0.03435 cm/ ms = 1/29.1)
+    // Distance = (duration from transmit to recieve/2)/29.1 
+    
+   distance = (duration/2) / 29.1;
+   
+   if (distance >= 200 || distance <= 0) {
+    Serial.println("Out of range");
+   }
+   else {
+    Serial.print(distance);
+    Serial.println(" cm");
+   }
+   delay(500);
 }
+
